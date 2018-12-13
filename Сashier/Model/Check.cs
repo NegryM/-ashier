26cabc40123c959987216331a5e5c28a226b3e12
NegryN <sateli@list.ru>
2018-12-13 10:37:48 +0300

    Init

---
+++ Сashier/Model/Check.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сashier.Model
+{
+   public class Check
+    {
+        private string number;
+        private decimal summ;
+        private int type;
+        private bool close;
+
+        public Check(string number)
+        {
+            this.number = number;
+            this.summ = 0;
+            this.type = 0;
+            this.close = false;
+        }
+    }
+}
